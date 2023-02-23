6201a3b07c98d9a9935e42f93621baa9b8bbd04f
Denzel Muñoz <a348802@uach.mx>
2023-02-23 17:55:04 -0600

    Archivos Agregados

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class MetodoNewton
+{
+    public static double raiz(double x)
+    {
+        double guess = x / 2;
+        double epsilon = 0.000001;
+        while (Math.Abs(guess * guess - x) > epsilon)
+        {
+            guess = (guess + x / guess) / 2;
+        }
+        return guess;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("***METODO NEWTON***");
+        Console.WriteLine("Introduce un valor númerico");
+        Console.ResetColor();
+        
+        string a = Console.ReadLine();
+
+    try
+    {
+        double x = Int64.Parse(a);
+        double raiz = MetodoNewton.raiz(x);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"La raíz cuadrada de {x} es {raiz}");
+        Main();
+    }
+    catch
+    {
+            Console.WriteLine("Introduce un valor valido");
+            Main();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Metodo Newton
+
+Programa para encontrar la raiz de un número por el metodo Newton 
+
+## Autor 
+
+Denzel Alexander Muñoz Santana 
+
+## Lenguaje de Programación 
+
+C#
+
+## IDE de Programación 
+
+Visual Studio Code
+
